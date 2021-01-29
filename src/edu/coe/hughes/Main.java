1a04977c4506d8063268c89ae986a70e0fb8c03d
SHughesCoe <shughes@coe.edu>
2021-01-29 15:20:53 -0600

    Updated Driver

--- src/edu/coe/hughes/Main.java
+++ src/edu/coe/hughes/Main.java
@@ -1,21 +1,20 @@
 package edu.coe.hughes;
 
 public class Main {
+	static final int GAMES_PLAYED = 10000000;
 
     public static void main(String[] args) {
-	Dice d1 = new Dice();
-	try {
+		Craps game = new Craps();
+		int wins = 0;
+		boolean victory;
 
-		Dice d2 = new Dice(7);
-
-		System.out.println(d1.roll());
-		System.out.println(d2.roll());
-
-		System.out.println(d1.getFace());
-		System.out.println(d2.getFace());
-	}
-	catch( IllegalArgumentException iae){
-		System.out.println("Bad dice Type");
-	}
+    	System.out.println("Playing Craps");
+		for (int i = 0; i < GAMES_PLAYED; i++){
+			victory = game.playGame();
+			if (victory){
+				wins++;
+			}
+		}
+		System.out.print("Winning percentage:  "+ (double)wins/GAMES_PLAYED);
     }
 }
